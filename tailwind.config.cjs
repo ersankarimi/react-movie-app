dfc57336ff214a93cd241901ca6baed53bf0c672
ersankarimi <ersankarimii311@gmail.com>
2022-10-13 02:08:46 +0800

    Update tailwind.config.cjs
    - Add new values in array content property
    - Add new value on the color inside extend property

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "main-color": "#121829",
+      },
+    },
   },
   plugins: [],
 };
