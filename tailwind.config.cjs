2f91ffc604e96120d0fba4497b69bb8f0df91a8b
ersankarimi <ersankarimii311@gmail.com>
2022-10-13 17:34:05 +0800

    Update tailwind.config.cjs
    Extends the screens property

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,14 @@ module.exports = {
       colors: {
         "main-color": "#121829",
       },
+      screens: {
+        xs: "475px",
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+      },
     },
   },
   plugins: [],
