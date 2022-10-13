fda226e4321015ec7049b7fe197ddeb063b018e9
ersankarimi <ersankarimii311@gmail.com>
2022-10-13 17:26:53 +0800

    Create SearchField.jsx

---
+++ src/components/SearchField.jsx
@@ -0,0 +1,40 @@
+import logo from "/src/assets/images/search.svg";
+
+import React, { useState } from "react";
+
+export const SearchField = () => {
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleChange = (event) => {
+    event.preventDefault();
+
+    setSearchValue(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    setSearchValue("");
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="flex items-center w-full gap-2 p-2 border-gray-700 border-[1px] rounded-lg bg-black/10 mt-4"
+    >
+      <label htmlFor="search" className="">
+        <img src={logo} alt="Search Icon" className="w-8 h-auto" />
+      </label>
+      <input
+        type="search"
+        name="search"
+        id="search"
+        className="appearance-none placeholder:text-gray-700 placeholder:text-[1em] rounded-lg p-2 focus:outline-none   w-full bg-transparent focus:bg-transparent"
+        value={searchValue}
+        autoComplete="off"
+        placeholder="Search Movie ..."
+        onChange={handleChange}
+      />
+    </form>
+  );
+};
